2561778d389a2074f77b570c045bc3372f43cbc0
EstebanM159 <estebanm91231@gmail.com>
2025-07-12 18:39:31 -0300

    feat(auth): implement authentication module with login and registration pages
    - Added AuthComponent to handle authentication routes.
    - Created LoginPageComponent and RegisterPageComponent for user login and registration.
    - Updated app.routes.ts to include authentication routes.
    - Integrated Tailwind CSS and DaisyUI for styling.
    - Removed placeholder content from app.component.html and replaced with router outlet.
    - Added global styles for Tailwind and DaisyUI in styles.css.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
+import { RegisterPageComponent } from './auth/pages/register-page/register-page.component';
+import { LoginPageComponent } from './auth/pages/login-page/login-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:'auth',
+    component:AuthComponent,
+    children:[
+      {path:'register',
+        component:RegisterPageComponent
+      },
+      {
+        path:'login',
+        component:LoginPageComponent
+      }
+    ]
+  },
+  {
+    path:'**',
+    redirectTo:'auth/login'
+  }
+];
